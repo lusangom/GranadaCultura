b16cffe3eb756dc65576665a0e06259333465b8a
luciaszgz <sanchl@usi.ch>
2024-03-19 19:36:28 +0100

    implementacion enfriamiento simulado ciclico

--- src/algoritmos/enfriamientosimulado.py
+++ src/algoritmos/enfriamientosimulado.py
@@ -3,12 +3,14 @@
 import pandas as pd
 import random
 import math
+import numpy as np
 
 MU = 0.3
 PHI = 0.2
 T_FINAL = 0.0001 # 10⁻⁴
 RANDOM_SEED = 36
 MAX_EVALUACIONES = 50000
+BETA = 0.2 
 
 class EnfriamientoSimulado:
     def __init__(self, nodos_df, distancias_df, tiempos_df, tiempo_max):
@@ -65,14 +67,59 @@ class EnfriamientoSimulado:
         # Devolver la lista de nodos visitados y el tiempo total
         return self.visitados, tiempo_actual, distancia_total, beneficio
     
-    def calcular_tiempo_distancia_total(self, solucion):
+    def generar_solucion_inicial_greedy_ciclico(self, nodo_ciclico):
+            self.visitados = [nodo_ciclico]
+            distancia_total = 0
+            tiempo_actual = self.nodos_df.loc[nodo_ciclico, 'tiempo_de_visita']
+            beneficio = self.nodos_df.loc[nodo_ciclico, 'interes']
+
+            while True:
+                mejor_fitness = -float('inf')
+                mejor_nodo = None
+                
+                for i, nodo in self.nodos_df.iterrows():
+                    if i not in self.visitados and i != nodo_ciclico:
+                        tiempo_vuelta = self.tiempos_df.loc[i, str(nodo_ciclico)]
+                        # Asegurarse de tener en cuenta el tiempo de visita en el nodo final
+                        tiempo_necesario = tiempo_actual + self.nodos_df.loc[i, 'tiempo_de_visita'] + tiempo_vuelta + self.nodos_df.loc[nodo_ciclico, 'tiempo_de_visita']
+                        
+                        if tiempo_necesario <= self.tiempo_max:
+                            fitness = self.calcular_fitness(self.visitados[-1], i)
+                            if fitness > mejor_fitness:
+                                mejor_fitness = fitness
+                                mejor_nodo = i
+                
+                if mejor_nodo is None:
+                    break
+
+                tiempo_viaje = self.tiempos_df.loc[self.visitados[-1], str(mejor_nodo)]
+                tiempo_actual += tiempo_viaje + self.nodos_df.loc[mejor_nodo, 'tiempo_de_visita']
+                distancia_total += self.distancias_df.loc[self.visitados[-1], str(mejor_nodo)]
+                beneficio += self.nodos_df.loc[mejor_nodo, 'interes']
+                self.visitados.append(mejor_nodo)
+
+            if tiempo_actual + self.tiempos_df.loc[self.visitados[-1], str(nodo_ciclico)] <= self.tiempo_max:
+                self.visitados.append(nodo_ciclico)
+                distancia_total += self.distancias_df.loc[self.visitados[-2], str(nodo_ciclico)]
+                tiempo_actual += self.tiempos_df.loc[self.visitados[-2], str(nodo_ciclico)]
+                # No se añade beneficio porque el nodo cíclico ya fue considerado al inicio
+
+            return self.visitados, tiempo_actual, distancia_total, beneficio
+    
+    def calcular_tiempo_total(self, solucion):
         tiempo_total = self.nodos_df.loc[solucion[0], 'tiempo_de_visita']
-        distancia_total = 0 
+        
         for i in range(len(solucion) - 1):
             tiempo_total += self.nodos_df.loc[solucion[i + 1], 'tiempo_de_visita']
             tiempo_total += self.tiempos_df.loc[solucion[i], str(solucion[i + 1])]
+            
+        return tiempo_total
+
+    def calcular_distancia_total(self, solucion):   
+        distancia_total = 0 
+        for i in range(len(solucion) - 1):
             distancia_total += self.distancias_df.loc[solucion[i], str(solucion[i + 1])]
-        return tiempo_total, distancia_total
+        return distancia_total
 
     def calcular_beneficio_total(self, solucion):
         beneficio_total = 0
@@ -81,59 +128,129 @@ class EnfriamientoSimulado:
             beneficio_total += self.nodos_df.loc[nodo, 'interes']
         return beneficio_total
     
+    
     def generar_vecino(self, solucion):
-        # Intentar encontrar un reemplazo válido que mantenga la solución dentro del tiempo máximo permitido
-        for intento in range(1):  # Limitar el número de intentos para evitar un bucle infinito
+        
         # Seleccionar un nodo aleatorio de la solución actual para ser reemplazado
-            nodo_a_reemplazar = random.choice(solucion)
+        nodo_a_reemplazar = random.choice(solucion)
 
             # Lista de nodos posibles para el reemplazo, excluyendo los ya presentes en la solución
-            nodos_posibles = [nodo for nodo in self.nodos_df.index if nodo not in solucion]
+        nodos_posibles = [nodo for nodo in self.nodos_df.index if nodo not in solucion]
 
             # Si no hay nodos disponibles para el reemplazo, devolver la solución actual sin cambios
-            if not nodos_posibles:
-                return solucion
+        if not nodos_posibles:
+            return solucion
 
             # Seleccionar un nuevo nodo de los nodos posibles para reemplazar en la solución
-            nuevo_nodo = random.choice(nodos_posibles)
+        nuevo_nodo = random.choice(nodos_posibles)
 
             # Crear una copia de la solución actual para modificarla
-            vecino_potencial = solucion[:]
-            index_a_reemplazar = vecino_potencial.index(nodo_a_reemplazar)  # Encontrar el índice del nodo a reemplazar
-            vecino_potencial[index_a_reemplazar] = nuevo_nodo  # Realizar el reemplazo
+        vecino_potencial = solucion[:]
+        index_a_reemplazar = vecino_potencial.index(nodo_a_reemplazar)  # Encontrar el índice del nodo a reemplazar
+        vecino_potencial[index_a_reemplazar] = nuevo_nodo  # Realizar el reemplazo
 
-            # Calcular el tiempo total de la solución potencial
-            tiempo_total = 0
-            for i, nodo in enumerate(vecino_potencial):
-                tiempo_total += self.nodos_df.loc[nodo, 'tiempo_de_visita']
-                if i > 0:  # Añadir el tiempo de viaje entre nodos, excepto para el primer nodo
-                    tiempo_total += self.tiempos_df.loc[vecino_potencial[i-1], str(nodo)]
+        
+
+        tiempo_total = self.calcular_tiempo_total(vecino_potencial)
+    
 
             # Comprobar si la solución propuesta cumple con el requisito de tiempo máximo
-            if tiempo_total <= self.tiempo_max:
-                return vecino_potencial  # Devolver la solución propuesta si es válida
+        if tiempo_total <= self.tiempo_max:
+            #print("En solucion original:",nodo_a_reemplazar)
+            #print("Insertamos:",nuevo_nodo)
+            #print("En posicion:",index_a_reemplazar)
+            return vecino_potencial  # Devolver la solución propuesta si es válida
             
-            intento=intento+1
-
-        # Si no se encuentra un reemplazo válido después de varios intentos, devolver la solución original
+           
+    
         return solucion
+    
+    def generar_vecino_ciclico(self, solucion):
+        # Asegurarse de que hay al menos tres nodos en la solución para excluir el primero y el último
+        if len(solucion) <= 3:
+            return solucion  # Devolver la solución actual sin cambios si no es posible excluir ambos
+
+        # Seleccionar un nodo aleatorio de la solución actual para ser reemplazado, excluyendo el primero y el último
+        nodo_a_reemplazar = random.choice(solucion[1:-1])
+
+        # Lista de nodos posibles para el reemplazo, excluyendo los ya presentes en la solución
+        nodos_posibles = [nodo for nodo in self.nodos_df.index if nodo not in solucion]
+
+        # Si no hay nodos disponibles para el reemplazo, devolver la solución actual sin cambios
+        if not nodos_posibles:
+            return solucion
+
+        # Seleccionar un nuevo nodo de los nodos posibles para reemplazar en la solución
+        nuevo_nodo = random.choice(nodos_posibles)
+
+        # Crear una copia de la solución actual para modificarla
+        vecino_potencial = solucion[:]
+        index_a_reemplazar = vecino_potencial.index(nodo_a_reemplazar)  # Encontrar el índice del nodo a reemplazar
+        vecino_potencial[index_a_reemplazar] = nuevo_nodo  # Realizar el reemplazo
+
+        # Calcular el tiempo total de la solución potencial
+        tiempo_total = self.calcular_tiempo_total(vecino_potencial)
+
+        # Comprobar si la solución propuesta cumple con el requisito de tiempo máximo
+        if tiempo_total <= self.tiempo_max:
+            return vecino_potencial  # Devolver la solución propuesta si es válida
+
+        return solucion
+
     
     def aplicar_enfriamiento_simulado(self):
         self.visitados, tiempo_actual, distancia_total, beneficio_actual = self.generar_solucion_inicial_greedy()
         
         t_actual = (MU * beneficio_actual) / (-math.log(PHI))
-        beta = 0.1  # Factor de enfriamiento
+        
+        
 
         iteracion = 0
         while t_actual > T_FINAL and iteracion < MAX_EVALUACIONES:
             vecino = self.generar_vecino(self.visitados)
             beneficio_vecino = self.calcular_beneficio_total(vecino)
-            delta_beneficio = beneficio_vecino - beneficio_actual
-            if delta_beneficio > 0 or random.random() < math.exp(delta_beneficio / t_actual):
+            tmp_act = self.calcular_tiempo_total(self.visitados)
+            tmp_vec = self.calcular_tiempo_total(vecino)
+            tmp_act = tmp_act*0.1
+            tmp_vec = tmp_vec*0.1
+            delta_beneficio = (beneficio_vecino - tmp_vec)  - (beneficio_actual - tmp_act)
+            if delta_beneficio > 0 or np.random.rand() < math.exp(delta_beneficio / t_actual):
+                #print("HACEMOS CAMBIO")
                 self.visitados = vecino
                 beneficio_actual = beneficio_vecino
-                tiempo_actual, distancia_total = self.calcular_tiempo_distancia_total(self.visitados)
-            t_actual = t_actual / (1 + beta * t_actual)
+                tiempo_actual = self.calcular_tiempo_total(self.visitados)
+                distancia_total = self.calcular_distancia_total(self.visitados)
+            #print(t_actual)    
+            t_actual = t_actual / (1 + BETA * t_actual)
+            iteracion += 1
+        
+        return self.visitados, tiempo_actual, distancia_total, beneficio_actual
+    
+    
+    def aplicar_enfriamiento_simulado_ciclico(self, nodo_ciclico):
+        self.visitados, tiempo_actual, distancia_total, beneficio_actual = self.generar_solucion_inicial_greedy_ciclico(nodo_ciclico=nodo_ciclico)
+        
+        t_actual = (MU * beneficio_actual) / (-math.log(PHI))
+        
+        
+
+        iteracion = 0
+        while t_actual > T_FINAL and iteracion < MAX_EVALUACIONES:
+            vecino = self.generar_vecino_ciclico(self.visitados)
+            beneficio_vecino = self.calcular_beneficio_total(vecino)
+            tmp_act = self.calcular_tiempo_total(self.visitados)
+            tmp_vec = self.calcular_tiempo_total(vecino)
+            tmp_act = tmp_act*0.1
+            tmp_vec = tmp_vec*0.1
+            delta_beneficio = (beneficio_vecino - tmp_vec)  - (beneficio_actual - tmp_act)
+            if delta_beneficio > 0 or np.random.rand() < math.exp(delta_beneficio / t_actual):
+                #print("HACEMOS CAMBIO")
+                self.visitados = vecino
+                beneficio_actual = beneficio_vecino
+                tiempo_actual = self.calcular_tiempo_total(self.visitados)
+                distancia_total = self.calcular_distancia_total(self.visitados)
+            #print(t_actual)    
+            t_actual = t_actual / (1 + BETA * t_actual)
             iteracion += 1
         
         return self.visitados, tiempo_actual, distancia_total, beneficio_actual
--- src/main.py
+++ src/main.py
@@ -16,9 +16,11 @@ def main():
     
     if nodos_df is not None and distancias_df is not None:
         
+        
         tiempo_max = int(input("Introduce el tiempo máximo: "))
         print("Tiempo máximo:", tiempo_max)
         
+        es_ciclica=False
         """ MAIN GREEDY
          # Crear una instancia del algoritmo
         alg_greedy = greedy.Greedy(nodos_df, distancias_df, tiempos_df, tiempo_max) 
@@ -59,7 +61,17 @@ def main():
         # Crear una instancia del algoritmo
         alg_es = enfriamientosimulado.EnfriamientoSimulado(nodos_df, distancias_df, tiempos_df, tiempo_max) 
         
-        ruta_solucion, tiempo_total, distancia_total, beneficio = alg_es.aplicar_enfriamiento_simulado()
+        es_ciclica = input("¿Deseas que la ruta sea cíclica? (si/no): ").strip().lower() == 'si'
+        nodo_origen = None
+
+        if es_ciclica:
+            print("Lista de POIs:")
+            for nodo, row in nodos_df.iterrows():
+                print(f"{nodo}: {row['name']}")
+            nodo_origen = int(input("Selecciona el número de nodo para el punto de origen: "))
+            ruta_solucion, tiempo_total, distancia_total, beneficio = alg_es.aplicar_enfriamiento_simulado_ciclico(nodo_ciclico=nodo_origen)
+        else:
+            ruta_solucion, tiempo_total, distancia_total, beneficio = alg_es.aplicar_enfriamiento_simulado()
        
         
         
@@ -68,21 +80,35 @@ def main():
         print("Tiempo total:", tiempo_total)   
         print("Distancia total:", distancia_total) 
         print("Interes total:", beneficio) 
-        print("Numero de nodos visitados:", len(ruta_solucion))
         print("Margen:", tiempo_max - tiempo_total)
-        print("Porcentaje interes: ",(beneficio/len(ruta_solucion))*10)
-        #print("Ruta ciclica:", es_ciclica)
+        if(es_ciclica):
+            
+            if(len(ruta_solucion)>1):
+                print("Porcentaje interes: ",(beneficio/(len(ruta_solucion)-1))*10)
+                print("Numero de nodos visitados:", len(ruta_solucion)-1)
+            else:
+                print("Porcentaje interes: ",(beneficio/(len(ruta_solucion)))*10)
+                print("Numero de nodos visitados:", len(ruta_solucion))
+           
+        else:
+            print("Numero de nodos visitados:", len(ruta_solucion)-1)
+            print("Porcentaje interes: ",(beneficio/len(ruta_solucion))*10)
+            
+        print("Ruta ciclica:", es_ciclica)
         
-        #vista = visualizacion.Visualizacion(nodos_df, ruta_solucion)
-        #if nodos_df is not None:
+       
+    
+        
+        vista = visualizacion.Visualizacion(nodos_df, ruta_solucion)
+        if nodos_df is not None:
         
 
-            #mapa_folium = vista.visualizar_ruta_en_mapa_folium(nodos_df)
+            mapa_folium = vista.visualizar_ruta_en_mapa_folium(nodos_df)
             #mapa_explore = vista.visualizar_ruta_en_mapa_explore(nodos_df)
             #vista.exportar_indicaciones_ruta_v1('indicaciones_ruta.txt')
 
 
-            #mapa_folium.save('ruta_solucion_folium.html')  # Guarda el mapa en un archivo HTML
+            mapa_folium.save('ruta_solucion_folium.html')  # Guarda el mapa en un archivo HTML
             #mapa_explore.save('ruta_solucion_explore.html')     
    
 if __name__ == "__main__":
